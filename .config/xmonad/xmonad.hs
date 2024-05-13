28c84096a9da844051c8f32ced84192ec43d604a
lumpsoid <mmgeeki@gmail.com>
2024-05-13 20:46:55 +0200

    added xmonad config

---
+++ .config/xmonad/xmonad.hs
@@ -0,0 +1,44 @@
+import XMonad
+
+import XMonad.Util.EZConfig -- basic config
+import XMonad.Layout.ThreeColumns -- layout
+import XMonad.Layout.Magnifier -- layout
+import XMonad.Hooks.EwmhDesktops -- fullscreenEventHook
+-- xmobar
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.ManageHook -- manageDocks
+
+
+main :: IO ()
+main = xmonad 
+      . ewmhFullscreen 
+      . ewmh 
+      . xmobarProp
+      . withEasySB (statusBarProp "xmobar" (pure xmobarPP)) defToggleStrutsKey -- defToggleStrutsKey = M-b
+      $ myConfig
+
+myConfig = def
+    { modMask = mod4Mask  -- Rebind Mod to the Super key
+    , layoutHook = myLayout  -- Use custom layouts
+    , manageHook = myManageHook
+    }  
+  `additionalKeysP`
+    [ ("M-f"  , spawn "librewolf"                   ),
+    ("M-Enter", spawn "st"),
+    ]
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
+    tiled   = Tall nmaster delta ratio
+    nmaster = 1      -- Default number of windows in the master pane
+    ratio   = 1/2    -- Default proportion of screen occupied by master pane
+    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ className =? "Gimp" --> doFloat
+    , isDialog            --> doFloat
+    ]
+
