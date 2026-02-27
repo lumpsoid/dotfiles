468b31fc845aadbdef5e139525876b4542f9e135
lumpsoid <79290581+lumpsoid@users.noreply.github.com>
2026-02-27 23:11:08 +0100

    xmonad: update

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,45 +1,82 @@
 import XMonad
 
-import XMonad.Util.EZConfig -- basic config
-import XMonad.Layout.ThreeColumns -- layout
-import XMonad.Layout.Magnifier -- layout
-import XMonad.Hooks.EwmhDesktops -- fullscreenEventHook
--- xmobar
-import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Magnifier
+import XMonad.Hooks.EwmhDesktops
+
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
-import XMonad.ManageHook -- manageDocks
+import XMonad.Util.Loggers
 
+import XMonad.Hooks.ManageDocks
+
+import XMonad.Actions.Promote
+
+import XMonad.Layout.MultiToggle as MultiToggle
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(FULL))
+
+myLayout = mkToggle (single FULL) $ (tiled ||| Mirror tiled ||| Full ||| threeCol)
+  where
+    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      
+    ratio    = 1/2    
+    delta    = 3/100
 
 main :: IO ()
-main = xmonad 
-      . ewmhFullscreen 
-      . ewmh 
-      . xmobarProp
-      . withEasySB (statusBarProp "xmobar" (pure xmobarPP)) defToggleStrutsKey -- defToggleStrutsKey = M-b
-      $ myConfig
+main = xmonad
+     . docks
+     . ewmhFullscreen
+     . ewmh
+     -- . withEasySB (statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
 
 myConfig = def
     { modMask = mod4Mask  -- Rebind Mod to the Super key
-    , layoutHook = myLayout  -- Use custom layouts
-    , manageHook = myManageHook
-    }  
-  `additionalKeysP`
-    [ ("M-f"  , spawn "librewolf"),                   ),
-    ("M-Enter", spawn "st"),
-    ("M-d", spawn "rofi -show run"),
+    , layoutHook = avoidStruts $ myLayout  -- Use custom layouts
+    }
+    `additionalKeysP`
+    [ ("M-w"  ,        spawn "brave"                             )
+    , ("M-<Return>",   spawn "kitty --single-instance"           )
+    , ("M-d"  ,        spawn "dmenu_run"                         )
+    , ("M-c"  ,        kill                                      )
+    , ("M-<Space>"  ,  promote                                   )
+    , ("M-u"  ,        sendMessage NextLayout                    )
+    , ("M-f",          sendMessage $ MultiToggle.Toggle FULL                 )
+    , ("<Print>", spawn "sc-printscreen")
+    , ("<XF86AudioRaiseVolume>", spawn "l--volume-control change 2%+")
+    , ("<XF86AudioLowerVolume>", spawn "l--volume-control change 2%-")
+    , ("<XF86AudioMute>",        spawn "l--volume-control toggle")
+
+    , ("<XF86MonBrightnessUp>",        spawn "brightnessctl s +2%")
+    , ("<XF86MonBrightnessDown>",        spawn "brightnessctl s 2%-")
     ]
-myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+
+myXmobarPP :: PP
+myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
   where
-    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
-    tiled   = Tall nmaster delta ratio
-    nmaster = 1      -- Default number of windows in the master pane
-    ratio   = 1/2    -- Default proportion of screen occupied by master pane
-    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
 
-myManageHook :: ManageHook
-myManageHook = composeAll
-    [ className =? "Gimp" --> doFloat
-    , isDialog            --> doFloat
-    ]
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
 
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
